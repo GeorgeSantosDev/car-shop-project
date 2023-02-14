13c4f7272ac002f432d88857093632046bf075f9
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-02-14 10:12:44 -0300

    test: Create afterEach and add find by id parameters

--- tests/unit/Services/car.test.ts
+++ tests/unit/Services/car.test.ts
@@ -3,10 +3,15 @@ import sinon from 'sinon';
 import { Model } from 'mongoose';
 import CarService from '../../../src/Services/car.service';
 import { carInput, carOutput, carsOutput } from '../../mocks/car.mocks';
+import HttpException from '../../../src/utils/HttpException';
 
 const service = new CarService();
 
 describe('Test car path', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
   it('should possible to register a new car', async function () {
     sinon.stub(Model, 'create').resolves(carOutput);
     const result = await service.register(carInput);
@@ -23,26 +28,26 @@ describe('Test car path', function () {
 
   it('should possible to get car by id', async function () {
     sinon.stub(Model, 'findById').resolves(carOutput);
-    const result = await service.findById();
+    const result = await service.findById('6348513f34c397abcad040b2');
 
     expect(result).to.be.deep.equal(carOutput);
   });
 
   it('should throw an error message Car not found when id does not exit in db', async function () {
-    sinon.stub(Model, 'findById').resolves({});
+    sinon.stub(Model, 'findById').resolves(null);
     try {
-      await service.findById();
+      await service.findById('6348513f34c397abcad040b2');
     } catch (error) {
-      expect((error as Error).message).to.be.equal('Car not found');
+      expect((error as HttpException).message).to.be.equal('Car not found');
     }
   });
 
   it('should throw an error message Invalid mongo id for invalid id format', async function () {
-    sinon.stub(Model, 'findById').resolves({});
+    sinon.stub(Model, 'findById').resolves();
     try {
-      await service.findById();
+      await service.findById('5');
     } catch (error) {
-      expect((error as Error).message).to.be.equal('Invalid mongo id');
+      expect((error as HttpException).message).to.be.equal('Invalid mongo id');
     }
   });
 });
