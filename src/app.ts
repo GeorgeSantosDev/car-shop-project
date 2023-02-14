35533cb43f289576721dca45fd14d68f28a8bc17
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-02-14 10:11:54 -0300

    feat: Apply error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import CarRoutes from './routes/carRoutes';
+import ErrorMiddleware from './middlewares/ErrorMiddleware';
+import HttpException from './utils/HttpException';
 
 const carRoutes = new CarRoutes();
 
@@ -8,5 +10,12 @@ const app = express();
 app.use(express.json());
 
 app.use('/cars', carRoutes.route);
+
+app.use((
+  err: HttpException,
+  req:express.Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => ErrorMiddleware.err(err, req, res, next));
 
 export default app;
