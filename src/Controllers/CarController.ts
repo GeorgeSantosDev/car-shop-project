1091f4dbcf6d9e73f5f44ffbff92b7caf20cba50
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-02-14 10:11:14 -0300

    feat: Create controller methods to get cars

--- src/Controllers/CarController.ts
+++ src/Controllers/CarController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { ICar } from '../Interfaces';
 import CarService from '../Services/car.service';
+import StatusCode from '../utils/StatusCode';
 
 const service = new CarService();
 
@@ -27,6 +28,24 @@ export default class CarController {
     const newCar = { model, year, color, status, buyValue, doorsQty, seatsQty };
 
     const response = await this._service.register(newCar);
-    return res.status(201).json(response);
+    return res.status(StatusCode.CREATED).json(response);
+  }
+
+  public async findAll(req: Request, res: Response, _next: NextFunction) {
+    const response = await this._service.findAll();
+
+    return res.status(StatusCode.SUCESS).json(response);
+  }
+
+  public async findById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+
+      const response = await this._service.findById(id);
+  
+      return res.status(StatusCode.SUCESS).json(response);
+    } catch (error) {
+      next(error);
+    }
   }
 }
