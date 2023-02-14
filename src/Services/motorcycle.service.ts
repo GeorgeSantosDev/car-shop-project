04db77f5b8fbf829b4ef54d05ba86ec69631d0a8
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-02-14 14:51:19 -0300

    feat: Create motorcycle service register method

---
+++ src/Services/motorcycle.service.ts
@@ -0,0 +1,22 @@
+import MotorcycleODM from '../Models/ MotorcycleODM';
+import AbstractODM from '../Models/AbstractODM';
+import { IMotorcycle } from '../Interfaces';
+import Motorcycle from '../Domains/ Motorcycle';
+
+const model = new MotorcycleODM();
+
+export default class MotorcycleService {
+  private _model: AbstractODM<IMotorcycle>;
+
+  constructor(motorcycleModel: AbstractODM<IMotorcycle> = model) {
+    this._model = motorcycleModel;
+  }
+
+  public async register(obj: IMotorcycle) {
+    const response = await this._model.create(obj);
+    if (response) {
+      return new Motorcycle(response);
+    }
+    return null;
+  }
+}
