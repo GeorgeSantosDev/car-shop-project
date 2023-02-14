984bbe058e774601c38ed958842b9f2a5ec04ebe
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-02-14 10:59:44 -0300

    test: Create tests for put method of cars path

--- tests/unit/Services/car.test.ts
+++ tests/unit/Services/car.test.ts
@@ -12,42 +12,78 @@ describe('Test car path', function () {
     sinon.restore();
   });
 
-  it('should possible to register a new car', async function () {
-    sinon.stub(Model, 'create').resolves(carOutput);
-    const result = await service.register(carInput);
+  describe('Test if is possible to create register new car', function () {
+    it('should possible to register a new car', async function () {
+      sinon.stub(Model, 'create').resolves(carOutput);
+      const result = await service.register(carInput);
 
-    expect(result).to.be.deep.equal(carOutput);
+      expect(result).to.be.deep.equal(carOutput);
+    });
   });
 
-  it('should possible to get all cars', async function () {
-    sinon.stub(Model, 'find').resolves(carsOutput);
-    const result = await service.findAll();
+  describe('Test if is possible to get cars', function () {
+    it('should possible to get all cars', async function () {
+      sinon.stub(Model, 'find').resolves(carsOutput);
+      const result = await service.findAll();
 
-    expect(result).to.be.deep.equal(carsOutput);
+      expect(result).to.be.deep.equal(carsOutput);
+    });
+
+    it('should possible to get car by id', async function () {
+      sinon.stub(Model, 'findById').resolves(carOutput);
+      const result = await service.findById('6348513f34c397abcad040b2');
+
+      expect(result).to.be.deep.equal(carOutput);
+    });
+
+    it(
+      'should throw an error message Car not found when id does not exit in db',
+      async function () {
+        sinon.stub(Model, 'findById').resolves(null);
+        try {
+          await service.findById('6348513f34c397abcad040b2');
+        } catch (error) {
+          expect((error as HttpException).message).to.be.equal('Car not found');
+        }
+      },
+    );
+
+    it('should throw an error message Invalid mongo id for invalid id format', async function () {
+      sinon.stub(Model, 'findById').resolves();
+      try {
+        await service.findById('5');
+      } catch (error) {
+        expect((error as HttpException).message).to.be.equal('Invalid mongo id');
+      }
+    });
   });
 
-  it('should possible to get car by id', async function () {
-    sinon.stub(Model, 'findById').resolves(carOutput);
-    const result = await service.findById('6348513f34c397abcad040b2');
+  describe('Test if is possible to update a car', function () {
+    it('should possible to update a car', async function () {
+      sinon.stub(Model, 'findByIdAndUpdate').resolves(carOutput);
+      const result = await service.update('6348513f34c397abcad040b2', carInput);
 
-    expect(result).to.be.deep.equal(carOutput);
-  });
+      expect(result).to.be.deep.equal(carOutput);
+    });
 
-  it('should throw an error message Car not found when id does not exit in db', async function () {
-    sinon.stub(Model, 'findById').resolves(null);
-    try {
-      await service.findById('6348513f34c397abcad040b2');
-    } catch (error) {
-      expect((error as HttpException).message).to.be.equal('Car not found');
-    }
-  });
+    it(
+      'should throw an error message Car not found when id does not exit in db',
+      async function () {
+        sinon.stub(Model, 'findByIdAndUpdate').resolves(null);
+        try {
+          await service.update('6348513f34c397abcad040b2', carInput);
+        } catch (error) {
+          expect((error as HttpException).message).to.be.equal('Car not found');
+        }
+      });
 
-  it('should throw an error message Invalid mongo id for invalid id format', async function () {
-    sinon.stub(Model, 'findById').resolves();
-    try {
-      await service.findById('5');
-    } catch (error) {
-      expect((error as HttpException).message).to.be.equal('Invalid mongo id');
-    }
+    it('should throw an error message Invalid mongo id for invalid id format', async function () {
+      sinon.stub(Model, 'findById').resolves();
+      try {
+        await service.update('5', carInput);
+      } catch (error) {
+        expect((error as HttpException).message).to.be.equal('Invalid mongo id');
+      }
+    });
   });
 });
