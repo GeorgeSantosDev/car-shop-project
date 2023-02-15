df2f9532cd4b427eabc350e77288b1c7e25217af
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-02-15 18:23:02 -0300

    refactor: Apply POO in app file

--- src/app.ts
+++ src/app.ts
@@ -7,18 +7,40 @@ import HttpException from './utils/HttpException';
 const carRoutes = new CarRoutes();
 const motorcycleRoutes = new MotorcycleRoutes();
 
-const app = express();
+class App {
+  public app: express.Express;
 
-app.use(express.json());
+  constructor() {
+    this.app = express();
 
-app.use('/cars', carRoutes.route);
-app.use('/motorcycles', motorcycleRoutes.route);
+    this.config();
+    this.routes();
+  }
 
-app.use((
-  err: HttpException,
-  req:express.Request,
-  res: express.Response,
-  next: express.NextFunction,
-) => ErrorMiddleware.err(err, req, res, next));
+  private routes(): void {
+    this.app.use('/cars', carRoutes.route);
+    this.app.use('/motorcycles', motorcycleRoutes.route);
+    this.app.use((
+      err: HttpException,
+      req:express.Request,
+      res: express.Response,
+      next: express.NextFunction,
+    ) => ErrorMiddleware.err(err, req, res, next));
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(express.json());
+    this.app.use(accessControl);
+  }
+}
+
+const { app } = new App();
 
 export default app;
